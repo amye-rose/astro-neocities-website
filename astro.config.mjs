42ec8d20d57933d8eff24f2edecbb1e510376c35
Amy Rose <amyerose2005@gmail.com>
2024-10-19 14:04:40 -0230

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://amyerose.xyz/"
+    site: "https://amye-rose.neocities.org/"
 });
